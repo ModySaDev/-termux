7445c0f0f102959af54c4276a47c410e8fa4c36a
Tech cochi <61864331+rixon-cochi@users.noreply.github.com>
2020-05-27 01:09:58 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+#installation
+
+clear
+
+toilet -f future "TELEGRAM" | lolcat
+echo ""
+echo ""
+echo ""
+xdg-open https://t.me/techcochiyoutube
+
+echo -e "\e[101m Press Enter \e[0m"
+read a1
+
+echo "Loading..." | lolcat
+sleep 1
+clear
+
+echo "Loading..." | lolcat
+sleep 1
+clear
+
+echo "Loading..." | lolcat
+sleep 1
+clear
+
+echo "Loading..." | lolcat
+sleep 1
+clear
+
+toilet -f future "TECH COCHI" | lolcat
+echo "-------------------------------------------------------" | lolcat
+echo   "Creadby  : TECH COCHI $white" |lolcat
+echo   "Contact  : Instagram.com/tech_code_2 $white" |lolcat
+echo   "YouTube  : TECH COCHI $white" |lolcat
+echo   "Website  : https://techcochi.unaux.com $white" |lolcat
+echo   "Telegram : https://t.me/techcochiyoutube $white" | lolcat
+echo "-------------------------------------------------------" | lolcat
+
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo  "Detected, Trying To Exit  ... "
+echo ""
+sleep 1
+echo ""
+echo "TECH-COCHI" | lolcat
+sleep 1
+exit
+}
+
+apt update
+apt upgrade -y
+pkg install ruby -y 
+gem install toilet -y 
+pkg install lolcat
+pkg install figlet
+pkg install git
+pkg install python2 -y 
+pkg install nano php -y
+pip2 install mechanize
+pip2 install --upgrade pip
+pip2 install requets
+
+echo ""
+echo "CODE_BY_TECH_COCHI | lolcat
+echo ""
+echo "________________________________________________________ | lolcat
