b989c4bdb4c75fade1743e2320407ed9fc888c97
Tech cochi <61864331+rixon-cochi@users.noreply.github.com>
2020-05-26 13:43:20 +0530

    Update b.sh

--- b.sh
+++ b.sh
@@ -49,21 +49,36 @@ while [ $lagi -lt 15 ];
 do
 echo ""
 echo -e "\e[1;101m\e[1;97m01\e[1;101m\e[0m\e[1;96m TOOL-X\e[0m\n";
+echo ""
 echo -e "\e[1;101m\e[1;97m02\e[1;101m\e[0m\e[1;96m LUCIFER\e[0m\n";
+echo ""
 echo -e "\e[1;101m\e[1;97m03\e[1;101m\e[0m\e[1;96m WEEMAN\e[0m\n";
+echo ""
 echo -e "\e[1;101m\e[1;97m04\e[1;101m\e[0m\e[1;96m RED_HAWK\e[0m\n";
+echo ""
 echo -e "\e[1;101m\e[1;97m05\e[1;101m\e[0m\e[1;96m TECH-HACH\e[0m\n";
+echo ""
 echo -e "\e[1;101m\e[1;97m06\e[1;101m\e[0m\e[1;96m BRUTEFORCE_FACEBOOK\e[0m\n";
-echo -e " \e[1;101m\e[1;97m07\e[1;101m\e[0m\e[1;96m BOT_KONEN_FACEBOOK\e[0m\n";
-echo -e " \e[1;101m\e[1;97m08\e[1;101m\e[0m\e[1;96m TERMUX-BASIC-PACKAGE\e[0m\n";
-echo -e " \e[1;101m\e[1;97m09\e[1;101m\e[0m\e[1;96m OSIF\e[0m\n";
-echo -e " \e[1;101m\e[1;97m10\e[1;101m\e[0m\e[1;96m INSTAGRAM-BRUTEFORCE\e[0m\n";
-echo -e " \e[1;101m\e[1;97m11\e[1;101m\e[0m\e[1;96m FIRECRACK\e[0m\n";
-echo -e " \e[1;101m\e[1;97m12\e[1;101m\e[0m\e[1;96m EVIL-EYE\e[0m\n";
-echo -e " \e[1;101m\e[1;97m13\e[1;101m\e[0m\e[1;96m TECH-CRASH\e[0m\n";
-echo -e " \e[1;101m\e[1;97m14\e[1;101m\e[0m\e[1;96m SPEED-X\e[0m\n";
-echo -e " \e[1;101m\e[1;97m15\e[1;101m\e[0m\e[1;96m SAYCHEESE\e[0m\n";
-echo -e " \e[1;101m\e[1;97m00\e[1;101m\e[0m\e[1;96m EXIT\e[0m\n";
+echo ""
+echo -e "\e[1;101m\e[1;97m07\e[1;101m\e[0m\e[1;96m BOT_KONEN_FACEBOOK\e[0m\n";
+echo ""
+echo -e "\e[1;101m\e[1;97m08\e[1;101m\e[0m\e[1;96m TERMUX-BASIC-PACKAGE\e[0m\n";
+echo ""
+echo -e "\e[1;101m\e[1;97m09\e[1;101m\e[0m\e[1;96m OSIF\e[0m\n";
+echo ""
+echo -e "\e[1;101m\e[1;97m10\e[1;101m\e[0m\e[1;96m INSTAGRAM-BRUTEFORCE\e[0m\n";
+echo ""
+echo -e "\e[1;101m\e[1;97m11\e[1;101m\e[0m\e[1;96m FIRECRACK\e[0m\n";
+echo ""
+echo -e "\e[1;101m\e[1;97m12\e[1;101m\e[0m\e[1;96m EVIL-EYE\e[0m\n";
+echo ""
+echo -e "\e[1;101m\e[1;97m13\e[1;101m\e[0m\e[1;96m TECH-CRASH\e[0m\n";
+echo ""
+echo -e "\e[1;101m\e[1;97m14\e[1;101m\e[0m\e[1;96m SPEED-X\e[0m\n";
+echo ""
+echo -e "\e[1;101m\e[1;97m15\e[1;101m\e[0m\e[1;96m SAYCHEESE\e[0m\n";
+echo ""
+echo -e "\e[1;101m\e[1;97m00\e[1;101m\e[0m\e[1;96m EXIT\e[0m\n";
 echo ""
 echo  "╭─TECH-COCHI" |lolcat
 read -p "╰──►" pil;
